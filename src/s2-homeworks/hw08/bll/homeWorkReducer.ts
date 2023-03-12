d75eca6183201f952f3f7d232c877aa2c4049c0c
Vadim Timofeev <uadik1970@mail.ru>
2023-03-12 18:03:41 +0300

    hw-08

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -1,18 +1,26 @@
-import {UserType} from '../HW8'
+import { UserType } from '../HW8'
 
 type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): any => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
-
-            return state // need to fix
+            // const sortByNameUp = (state:UserType[]) => {
+            //     return (
+            //         state.a.name > state.b.name ? 1 : -1
+            //     )
+            // }
+            return [...state.sort((a: UserType, b: UserType) => {
+                return action.payload === 'up'
+                    ? a.name > b.name ? 1 : -1
+                    : a.name < b.name ? 1 : -1
+            })] // need to fix
         }
         case 'check': {
 
-            return state // need to fix
+            return state.filter(el => el.age >= 18)
         }
         default:
             return state
